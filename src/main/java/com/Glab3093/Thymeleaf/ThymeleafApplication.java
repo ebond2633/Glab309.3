781a99179cdee30de16ca1625054df685315317c
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-08-14 16:10:05 -0400

    Glab309.3

---
+++ src/main/java/com/Glab3093/Thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.Glab3093.Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafApplication.class, args);
+	}
+
+}
